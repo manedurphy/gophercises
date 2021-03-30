cba0bfd1f4a2ec9e2b3ceb5cc1097fbdb45e0fce
Dane Murphy <danem7@gmail.com>
2021-03-29 19:22:51 -0700

    styling added

--- exercise3/story.go
+++ exercise3/story.go
@@ -3,8 +3,10 @@ package exercise3
 import (
 	"encoding/json"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type Story map[string]Chapter
@@ -34,17 +36,31 @@ func GetDecodedJson(file *os.File) (Story, error) {
 	return story, nil
 }
 
-func NewHandler(s Story) handler {
+func NewHandler(s Story) http.Handler {
 	return handler{s}
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	tpl := template.Must(template.New("").Parse(htmlTemplate))
+	path := strings.TrimSpace(r.URL.Path)
 
-	err := tpl.Execute(w, h.s["intro"])
-	if err != nil {
-		panic(err)
+	if path == "" || path == "/" {
+		path = "/intro"
 	}
+
+	path = path[1:]
+
+	if chapter, ok := h.s[path]; ok {
+		err := tpl.Execute(w, chapter)
+
+		if err != nil {
+			log.Printf("%+v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+
+		return
+	}
+	http.Error(w, "Chapter was not found...", http.StatusNotFound)
 }
 
 var htmlTemplate string = `
@@ -55,20 +71,62 @@ var htmlTemplate string = `
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Choose Your Own Adventure</title>
+	<style>
+		body {
+		  font-family: helvetica, arial;
+		}
+		h1 {
+		  text-align:center;
+		  position:relative;
+		}
+		.page {
+		  width: 80%;
+		  max-width: 500px;
+		  margin: auto;
+		  margin-top: 40px;
+		  margin-bottom: 40px;
+		  padding: 80px;
+		  background: #FFFCF6;
+		  border: 1px solid #eee;
+		  box-shadow: 0 10px 6px -6px #777;
+		}
+		ul {
+		  border-top: 1px dotted #ccc;
+		  padding: 10px 0 0 0;
+		  -webkit-padding-start: 0;
+		}
+		li {
+		  padding-top: 10px;
+		}
+		a,
+		a:visited {
+		  text-decoration: none;
+		  color: #6295b5;
+		}
+		a:active,
+		a:hover {
+		  color: #7792a2;
+		}
+		p {
+		  text-indent: 1em;
+		}
+	  </style>
 </head>
 <body>
-	<h1>{{.Title}}</h1>
-	{{range .Paragraphs}}
-	<p>{{.}}</p>
-	{{end}}
-	<ul>
-		{{range .Options}}
-		{{.}}
-		<li>
-			<a href="/{{.Chapter}}">{{.Text}}</a>
-		</li>
+	<section class="page">
+		<h1>{{.Title}}</h1>
+		{{range .Paragraphs}}
+		<p>{{.}}</p>
 		{{end}}
-	</ul>
+		<ul>
+			{{range .Options}}
+			{{.}}
+			<li>
+				<a href="/{{.Chapter}}">{{.Text}}</a>
+			</li>
+			{{end}}
+		</ul>
+	</section>
 </body>
 </html>
 `
