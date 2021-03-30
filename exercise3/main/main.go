7df3c0c44052f481a020c5c3414301a6cb715497
Dane Murphy <danem7@gmail.com>
2021-03-29 17:42:55 -0700

    json story parser

---
+++ exercise3/main/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	jsonFileName := flag.String("json", "gopher.json", "a json file with story arcs")
+	flag.Parse()
+
+	file, err := os.Open(*jsonFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := storyJson.GetDecodedJson(file)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v", story)
+}
+
+// type Story map[string]Chapter
+
+// type Chapter struct {
+// 	Title      string   `json:"title"`
+// 	Paragraphs []string `json:"story"`
+// 	Options    []Option `json:"options"`
+// }
+
+// type Option struct {
+// 	Text    string `json:"text"`
+// 	Chapter string `json:"arc"`
+// }
+
+// func getDecodedJson(file *os.File) (Story, error) {
+// 	d := json.NewDecoder(file)
+
+// 	var story Story
+// 	if err := d.Decode(&story); err != nil {
+// 		return nil, err
+// 	}
+// 	return story, nil
+// }
---
+++ exercise3/story.go
@@ -0,0 +1,29 @@
+package storyJson
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
+}
+
+func GetDecodedJson(file *os.File) (Story, error) {
+	d := json.NewDecoder(file)
+
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
