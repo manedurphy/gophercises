1955af8d495eeb4ac2df6ecf81ade37a8bba4377
Dane Murphy <danem7@gmail.com>
2021-03-29 21:06:52 -0700

    exercise 3 complete

--- exercise3/story.go
+++ exercise3/story.go
@@ -23,8 +23,23 @@ type Option struct {
 }
 
 type handler struct {
-	s Story
-	t *template.Template
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
+}
+
+type HandlerOption func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
 }
 
 var tpl *template.Template = template.Must(template.New("").Parse(htmlTemplate))
@@ -39,24 +54,31 @@ func GetDecodedJson(file *os.File) (Story, error) {
 	return story, nil
 }
 
-func NewHandler(s Story, t *template.Template) http.Handler {
-	if t == nil {
-		t = tpl
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+
+	for _, opt := range opts {
+		opt(&h)
 	}
-	return handler{s, t}
+
+	return h
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
 
-	path = path[1:]
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 
 	if chapter, ok := h.s[path]; ok {
-		err := tpl.Execute(w, chapter)
+		err := h.t.Execute(w, chapter)
 
 		if err != nil {
 			log.Printf("%+v", err)
